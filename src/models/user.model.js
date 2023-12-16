b88cd8b1f50cd051d67fc398bfaada309c86f8b1
AKACHI-4 <adarshrawat8304@gmail.com>
2023-12-16 22:15:30 +0530

    third commit

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -64,17 +64,31 @@ userSchema.methods.isPasswordCorrect = async function(password) {
 
 userSchema.methods.generateAccessToken = function(){
   // payload
-  jwt.sign(
+  return jwt.sign(
     {
       _id: this._id,
       email: this.email,
       username: this.username,
       fullname: this.fullname
+    },
+    process.env.ACCESS_TOKEN_SECRET, 
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY
     }
   )
 }
-userSchema.methods.generateRefreshToken = function(){
 
+// less payload then access token
+userSchema.methods.generateRefreshToken = function(){
+  return jwt.sign(
+    {
+      _id: this._id, 
+    }, 
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )
 }
 
 /*

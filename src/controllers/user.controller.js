cdb85b70d4fd336f0660fc95cc2347406ab2708d
AKACHI-4 <Adarshrawat8304@gmail.com>
2023-12-20 23:01:37 +0530

    test controller and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,75 +4,73 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinaryService.js";
 import { apiResponse } from "../utils/apiResponse.js";
 
-const registerUser = asyncHandler(async (req, res) => { 
-    // get user details from frontend
-    // validation - not empty (common validation)
-    // check user already exists: by username and email both
-    // check for images
-    // check for avatar
-    // upload them cloudinary, avatar check
-    // create user object -- in mongo db nosql database objects transferred
-    // remove password and refresh token field from response
-    // check for user creation
-    // return res
-    // -- or isko bolte hain logic building
+const registerUser = asyncHandler(async (req, res) => {
+  // get user details from frontend
+  // validation - not empty (common validation)
+  // check user already exists: by username and email both
+  // check for images
+  // check for avatar
+  // upload them cloudinary, avatar check
+  // create user object -- in mongo db nosql database objects transferred
+  // remove password and refresh token field from response
+  // check for user creation
+  // return res
+  // -- or isko bolte hain logic building
 
-    const {fullname, email, username, password} = req.body
-    
-    if(
-        [fullname, email, username, password].some((field) => field?.trim() === "")
-    ) {
-        throw new apiError(400, "All fields are Required!!");
-    }
+  const { fullname, email, username, password } = req.body;
 
-    const existedUser = User.findOne({
-        $or: [
-            {
-                username
-            },
-            {
-                email
-            }
-        ]
-    })
-    
-    if(existedUser) {
-        throw new apiError(409, "User already Exists!!");
-    }
-    console.log(existedUser);
+  if (
+    [fullname, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new apiError(400, "All fields are Required!!");
+  }
 
-    console.log(req.files);
-    const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  const existedUser = await User.findOne({
+    $or: [
+      {
+        username,
+      },
+      {
+        email,
+      },
+    ],
+  });
 
-    if (!avatarLocalPath)
-        throw new apiError(400, "User Avatar is Required !!");
+  if (existedUser) {
+    throw new apiError(409, "User already Exists!!");
+  }
+  console.log(existedUser);
 
-    const avatar = await uploadOnCloudinary(avatarLocalPath);
-    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+  console.log(req.files);
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
-    if (!avatar) 
-        throw new apiError(400, "User Avatar is Required !!");
+  if (!avatarLocalPath) throw new apiError(400, "User Avatar is Required !!");
 
-    const user = await User.create({
-        fullname,
-        avatar: avatar.url, 
-        coverImage: coverImage.url || "",
-        email, 
-        password,
-        username: username.toLowerCase()
-    })
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
-    const theUser = await User.findById(user._id).select(
-        "-password -refreshToken"
-    )
-    
-    if (!createdUser) 
-        throw new apiError(500, "Something went wrong while registering the user.");
+  if (!avatar) throw new apiError(400, "User Avatar is Required !!");
 
-    return res.status(201).json(
-        new apiResponse(200, createdUser, "User successfully registered!!")
-    )
-})
+  const user = await User.create({
+    fullname,
+    avatar: avatar.url,
+    coverImage: coverImage.url || "",
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
+
+  const theUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!theUser)
+    throw new apiError(500, "Something went wrong while registering the user.");
+
+  return res
+    .status(201)
+    .json(new apiResponse(200, theUser, "User successfully registered!!"));
+});
 
 export { registerUser };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,6 @@
 import mongoose, { Schema } from "mongoose";
-import jwt from 'jsonwebtoken'; 
-import bcrypt from 'bcrypt';
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
@@ -34,7 +34,7 @@ const userSchema = new Schema(
     },
     watchHistory: [
       {
-        types: Schema.Types.ObjectId,
+        type: Schema.Types.ObjectId,
         ref: "Video",
       },
     ],
@@ -51,45 +51,45 @@ const userSchema = new Schema(
   }
 );
 
-userSchema.pre("save", async function(next) {
-  if (!this.isModified('password')) return next();
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
 
   this.password = await bcrypt.hash(this.password, 10);
   next();
-})
+});
 
-userSchema.methods.isPasswordCorrect = async function(password) {
+userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
-}
+};
 
-userSchema.methods.generateAccessToken = function(){
+userSchema.methods.generateAccessToken = function () {
   // payload
   return jwt.sign(
     {
       _id: this._id,
       email: this.email,
       username: this.username,
-      fullname: this.fullname
+      fullname: this.fullname,
     },
-    process.env.ACCESS_TOKEN_SECRET, 
+    process.env.ACCESS_TOKEN_SECRET,
     {
-      expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
     }
-  )
-}
+  );
+};
 
 // less payload then access token
-userSchema.methods.generateRefreshToken = function(){
+userSchema.methods.generateRefreshToken = function () {
   return jwt.sign(
     {
-      _id: this._id, 
-    }, 
+      _id: this._id,
+    },
     process.env.REFRESH_TOKEN_SECRET,
     {
-      expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
     }
-  )
-}
+  );
+};
 
 /*
   jwt is bearer token .. means whomsever bear it will be its owner.. so whenever that user needs the data and something .. server send it to him/her.
